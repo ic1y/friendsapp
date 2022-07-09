26e75182ba522746669880ea4d257df55bdc48fa
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-07-09 11:24:58 +0800

    added ",".

--- friendsapp/friends.swift
+++ friendsapp/friends.swift
@@ -30,6 +30,6 @@ struct Friend: Identifiable {
 var friendsArray = [Friend(name: "Vanisha", age: 13, zodiacSign: .gemini, MBTI: .ENFJ, school: "RGS"),
                     Friend(name: "Katelyn", age: 14, zodiacSign: .capricorn, MBTI: .INFJ, school: "RGS"),
                     Friend(name: "Zhiyu", age: 1, zodiacSign: .libra, MBTI: .ESTJ, school: "Sparkletots"),
-                    Friend(name: "James", age: 15, zodiacSign: .scorpio, MBTI: .INTP, school: "CCSS")
+                    Friend(name: "James", age: 15, zodiacSign: .scorpio, MBTI: .INTP, school: "CCSS"),
                     Friend(name: "Daniyal", age: 15, zodiacSign: .sagittarius, MBTI: .ISTJ, school: "SPS")
 ]
