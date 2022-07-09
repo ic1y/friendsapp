2d2cb56ed2cf862642602886a859695392c65ce5
Ice <icey.ggjt@gmail.com>
2022-07-09 10:42:51 +0800

    Initial Commit

---
+++ friendsapp/friendsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendsappApp.swift
+//  friendsapp
+//
+//  Created by Ice on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct friendsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
