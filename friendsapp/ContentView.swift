973b5b81fea6191e40127a97d05716332b669ae2
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-07-09 11:35:03 +0800

    foreach from katelyn

--- friendsapp/ContentView.swift
+++ friendsapp/ContentView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var friends = ""
+    @State var friends = [
+        Friend(name: "Vanisha", age: 13, zodiacSign: .gemini, MBTI: .ENFJ, school: "RGS"),
+        Friend(name: "Katelyn", age: 14, zodiacSign: .capricorn, MBTI: .INFJ, school: "RGS"),
+        Friend(name: "Zhiyu", age: 1, zodiacSign: .libra, MBTI: .ESTJ, school: "Sparkletots"),
+        Friend(name: "James", age: 15, zodiacSign: .scorpio, MBTI: .INTP, school: "CCSS"),
+        Friend(name: "Daniyal", age: 15, zodiacSign: .sagittarius, MBTI: .ISTJ, school: "SPS"),
+        Friend(name: "Gilbert", age: 14, zodiacSign: .scorpio, MBTI: .INTP, school: "NCHS")
+    ]
     var body: some View {
-        Text("Friends App2222")
-            .padding()
+        List{
+            ForEach($friends) {$friend in
+                Text(friend.name)
+            }
+        }
     }
 }
 
